ab5f25c503559db738ad47bba18108ef247948eb
MichaelDelRosarioJr <38480572+MichaelDelRosarioJr@users.noreply.github.com>
2019-07-19 17:15:15 -0500

    Add files via upload

---
+++ Asg5_mxd120830/Asg5_mxd120830/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Asg5_mxd120830
+{
+    partial class MultiThreaded_Text_Search
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileLabel = new System.Windows.Forms.Label();
+            this.searchLabel = new System.Windows.Forms.Label();
+            this.fileNametextBox = new System.Windows.Forms.TextBox();
+            this.searchtextBox = new System.Windows.Forms.TextBox();
+            this.searchListView = new System.Windows.Forms.ListView();
+            this.lineColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.stringColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.browseButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusStrip = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileLabel
+            // 
+            this.fileLabel.AutoSize = true;
+            this.fileLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileLabel.Location = new System.Drawing.Point(14, 12);
+            this.fileLabel.Name = "fileLabel";
+            this.fileLabel.Size = new System.Drawing.Size(100, 25);
+            this.fileLabel.TabIndex = 0;
+            this.fileLabel.Text = "File Name";
+            this.fileLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // searchLabel
+            // 
+            this.searchLabel.AutoSize = true;
+            this.searchLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchLabel.Location = new System.Drawing.Point(14, 58);
+            this.searchLabel.Name = "searchLabel";
+            this.searchLabel.Size = new System.Drawing.Size(75, 25);
+            this.searchLabel.TabIndex = 1;
+            this.searchLabel.Text = "Search";
+            // 
+            // fileNametextBox
+            // 
+            this.fileNametextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileNametextBox.Location = new System.Drawing.Point(120, 12);
+            this.fileNametextBox.Name = "fileNametextBox";
+            this.fileNametextBox.Size = new System.Drawing.Size(959, 30);
+            this.fileNametextBox.TabIndex = 2;
+            // 
+            // searchtextBox
+            // 
+            this.searchtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchtextBox.Location = new System.Drawing.Point(120, 58);
+            this.searchtextBox.Name = "searchtextBox";
+            this.searchtextBox.Size = new System.Drawing.Size(959, 30);
+            this.searchtextBox.TabIndex = 3;
+            // 
+            // searchListView
+            // 
+            this.searchListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lineColumn,
+            this.stringColumn});
+            this.searchListView.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchListView.Location = new System.Drawing.Point(18, 94);
+            this.searchListView.Name = "searchListView";
+            this.searchListView.Size = new System.Drawing.Size(1165, 351);
+            this.searchListView.TabIndex = 4;
+            this.searchListView.UseCompatibleStateImageBehavior = false;
+            this.searchListView.View = System.Windows.Forms.View.Details;
+            // 
+            // lineColumn
+            // 
+            this.lineColumn.Text = "Line #";
+            this.lineColumn.Width = 65;
+            // 
+            // stringColumn
+            // 
+            this.stringColumn.Text = "Text";
+            this.stringColumn.Width = 1095;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browseButton.Location = new System.Drawing.Point(1085, 12);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(98, 30);
+            this.browseButton.TabIndex = 5;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchButton.Location = new System.Drawing.Point(1085, 58);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(98, 30);
+            this.searchButton.TabIndex = 6;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusStrip});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 458);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1195, 25);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(252, 20);
+            this.statusStrip.Text = "Please Enter File and Word to Search";
+            this.statusStrip.Click += new System.EventHandler(this.statusStrip_Click);
+            // 
+            // MultiThreaded_Text_Search
+            // 
+            this.AcceptButton = this.searchButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1195, 483);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.searchListView);
+            this.Controls.Add(this.searchtextBox);
+            this.Controls.Add(this.fileNametextBox);
+            this.Controls.Add(this.searchLabel);
+            this.Controls.Add(this.fileLabel);
+            this.Name = "MultiThreaded_Text_Search";
+            this.Text = "Text Search Program";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fileLabel;
+        private System.Windows.Forms.Label searchLabel;
+        private System.Windows.Forms.TextBox fileNametextBox;
+        private System.Windows.Forms.TextBox searchtextBox;
+        private System.Windows.Forms.ListView searchListView;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusStrip;
+        private System.Windows.Forms.ColumnHeader lineColumn;
+        private System.Windows.Forms.ColumnHeader stringColumn;
+    }
+}
+
---
+++ Asg5_mxd120830/Asg5_mxd120830/Form1.cs
@@ -0,0 +1,198 @@
+﻿/******************************************************************************
+ * Assignment 5 - Multithreaded Text Search
+ *
+ * This program opens Dante's Inferno text file, if file path exits, and starts a
+ * search thread to search for the string input by the user in the file.
+ * The search is insensitive with a 1 millisecond delay for every line read.
+ * The user can cancel search at any time.
+ *
+ * Written by Michael Del Rosario (mxd120830)
+ * Assignment 5, starting November 8, 2017.
+ ******************************************************************************/
+
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Asg5_mxd120830
+{
+    public partial class MultiThreaded_Text_Search : Form
+    {
+        public MultiThreaded_Text_Search()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /*
+         * Browse button opens an additional window that allows user to insert file path automatically.
+         */
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            //Create a dialogue box to search file
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            //Write file path to file path text box
+            if (result == DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName;
+                fileNametextBox.Text = file;
+            }
+        }
+        
+        //Create a global thread that stores the search thread
+        private Thread searchThread;
+        private ThreadStart searchThreadStart;
+
+        /*
+         * SearchButton_Click starts the searchStringThread and cancels thread if thread is currently running
+         */ 
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            //Check to see what the button text is
+            if (searchButton.Text.Contains("Search"))//If search has not started, start search thread and change button
+            {
+                //Initialize thread
+                searchThreadStart = new ThreadStart(searchStringThread);
+                searchThread = new Thread(searchThreadStart);
+
+                //Clear searchListView for new Search
+                searchListView.Items.Clear();
+
+                //Start search Thread
+                searchThread.Start();
+
+                
+                
+            }
+            if(searchButton.Text.Contains("Cancel"))//If search is in progress, cancel search thread
+            {
+                //Cancel search Thread
+                searchThread.Abort();              
+            }
+
+            //Switch Button Text when search starts
+            if(searchButton.Text.Contains("Search"))
+                searchButton.Text = "Cancel";
+        }
+
+        /*
+         * searchStringThread opens text file if file exists, or end thread otherwise.
+         * Funtion reads file line by line, and print the line number and line everytime the string
+         * from the searchTextBox is contained within the line. The search is insensitive and finds any
+         * insance of the substring of searchTextBox. Thread records current state while running and end state before closing.
+         */ 
+        private void searchStringThread()
+        {
+           try
+            {
+                //Check to see if file exists
+                string path = fileNametextBox.Text;
+                
+                //If file is not found set, end search
+                if (!File.Exists(path))
+                { 
+                    statusStrip.Text = "Error! File does not exist or File path is incorrect. Search ended.";
+                    
+                }
+                else//If file exits
+                {
+                    //Create file with filepath
+                    FileInfo searchFile = new FileInfo(path);
+                    statusStrip.Text = "File Found. Starting thread.";
+
+                    //Store search string
+                    string searchText = searchtextBox.Text;
+                    int lineNum = 1; //Stores the current line number.
+
+                    
+
+                    using (StreamReader r = searchFile.OpenText())
+                    {
+                        
+                        
+
+                        string currentLine = "";//Stores the current line
+                        StringComparison compare = StringComparison.OrdinalIgnoreCase;//For insensitive searching
+
+                        //Read file line by line and print any line that contains searchText
+                        while ((currentLine = r.ReadLine()) != null)
+                        {
+                            //Give current status information
+                            statusStrip.Text = lineNum + "/" + (File.ReadAllLines(path).Length + 1) + " lines remaining. " + "Searching Text...     ";
+
+                            //If the current line contains the search text, print line number and line on listView.
+                            if (currentLine.IndexOf(searchText, compare)>=0)
+                            {
+                                //Create new item row for listView via string array
+                                string[] row = { lineNum.ToString(), currentLine };
+                                ListViewItem insrt = new ListViewItem(row);
+                                
+                                //Allow thread to add to listView
+                                if (searchListView.InvokeRequired)
+                                {
+                                    searchListView.BeginInvoke(new MethodInvoker(() => searchListView.Items.Add(insrt)));
+                                }
+                                else
+                                    searchListView.Items.Add(insrt);
+
+
+                                //TEST: Write line to console
+                                //Console.WriteLine(lineNum.ToString() + " " + t);
+                            }
+
+                            lineNum++;//Increment lineNum
+                            Thread.Sleep(1);//Wait for a millisecond
+
+                            //TEST: Put Thread to Extended Sleep to test Cancel Button
+                            //Thread.Sleep(1000);
+                        }
+                    }
+
+                    
+
+                    //Inform user that the search has concluded
+                    statusStrip.Text = "Search complete. Search ended";
+                }
+
+                
+
+                
+            }
+            catch (ThreadAbortException e) //Tell User That Thread was cancelled
+            {
+                statusStrip.Text = "Search cancelled. Search ended";
+            }
+            finally
+            {
+                //Allow thread to change cancel button back to search
+                if (searchButton.InvokeRequired)
+                    searchListView.BeginInvoke(new MethodInvoker(() => searchButton.Text = "Search"));
+                else
+                    searchButton.Text = "Search";
+            }
+         }
+
+        private void statusStrip_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
